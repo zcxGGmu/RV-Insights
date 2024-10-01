21d4c8fc6f29e1574c29b2f55873951a43943722
zcxGGmu <zhouquan@iscas.ac.cn>
2024-10-01 09:58:03 +0800

    server: Implement a basic utility library for connecting to LLMs
    Currently includes the following categories of large models: qwen/ernie/zhipu/baichuan
    
    Signed-off-by: zcxGGmu <zhouquan@iscas.ac.cn>

---
+++ server/settings.py
@@ -0,0 +1,62 @@
+import os
+from utils.util import load_qwen_models
+from utils.util import load_baichuan_models
+from utils.util import load_zhipu_models
+from utils.util import load_ernie_models
+from utils.util import load_qwen_embeddings
+from utils.util import load_zhipu_embeddings
+from utils.util import load_ernie_embeddings
+
+""" 
+# Alibaba Qwen Series Models
+# The default model is qwen-max
+#   Model usage - Input: 0.04/text_token (per thousand)
+#   Model usage - Output: 0.12/text_token (per thousand)
+# qwen-long model
+#   Model usage - Input: 0.0005/text_token (per thousand)
+#   Model usage - Output: 0.002/text_token (per thousand)
+"""
+# LLM = load_qwen_models(model="qwen-max")[0]
+# CHAT = load_qwen_models(model="qwen-max")[1]
+# EMBED = load_qwen_embeddings()
+
+"""
+# Baidu ERNIE Bot Series Models
+# Default Model: ERNIE-Bot-turbo
+#   Free
+# ERNIE-4.0-8K Model
+#   Model usage - Input: ¥0.04/1000 tokens
+#   Model usage - Output: ¥0.12/1000 tokens
+# ERNIE-3.5-8K Model
+#   Model usage - Input: ¥0.004/1000 tokens
+#   Model usage - Output: ¥0.012/1000 tokens
+"""
+# LLM = load_ernie_models()[0]
+# CHAT = load_ernie_models()[1]
+# EMBED = load_ernie_embeddings()
+
+"""
+# Zhipu Dialogue Models
+# Promotion: Register to receive 5 million tokens,
+#            new customer exclusive offer - recharge ¥99 for 10 million tokens
+# GLM-4-Plus Model
+#   Price: ¥0.05/1000 tokens
+# GLM-4-Air Model
+#   Price: ¥0.001/1000 tokens
+#   Batch API Pricing: ¥0.0005/1000 tokens
+"""
+# LLM = load_zhipu_models(model="glm-4-plus")
+# CHAT = load_zhipu_models(model="glm-4-flash")
+# EMBED = load_zhipu_embeddings(model="embedding-3")
+
+"""
+Chroma
+"""
+
+"""
+Milvus
+"""
+
+"""
+ESDB
+"""
