9f1213171e0fa9bd82b31b3d6aa48571f9abab9b
zcxGGmu <zhouquan@iscas.ac.cn>
2024-10-08 08:05:00 +0800

    tiny change

---
+++ server/rag/vector_db.py
@@ -0,0 +1,19 @@
+import chromadb
+from langchain_chroma import Chroma
+from langchain_community.vectorstores.milvus import Milvus
+
+class VectorDB:
+    def add_with_langchain(self, docs):
+        """
+        Add documents to the vector database using LangChain.
+        """
+        raise NotImplementedError("Subclasses should implement this method")
+
+    def get_store(self):
+        """
+        Get the vector database store.
+        """
+        raise NotImplementedError("Subclasses should implement this method")
+
+
+
